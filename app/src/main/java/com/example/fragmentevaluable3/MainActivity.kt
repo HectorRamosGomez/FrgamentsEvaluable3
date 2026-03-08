436e2d4ed1f51a71f92177d222d17b36629e56e7
Hector Ramos Gomez <hectorramosgomez717@gmail.com>
2026-03-08 21:37:05 +0100

    Funcion para cambiar de fragment

--- app/src/main/java/com/example/fragmentevaluable3/MainActivity.kt
+++ app/src/main/java/com/example/fragmentevaluable3/MainActivity.kt
@@ -5,11 +5,24 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.example.fragmentevaluable3.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
 
+    private fun remplazarFragement(fragment: Fragment){
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.FrameLayout, fragment)
+        fragmentTransaction.commit()
     }
 }
