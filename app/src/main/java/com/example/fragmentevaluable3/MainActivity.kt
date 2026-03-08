bb9f84f1ab463f86439d77431e8fd3256c9901ca
Hector Ramos Gomez <hectorramosgomez717@gmail.com>
2026-03-09 00:56:03 +0100

    proyecto arreglado hasta ahora

--- app/src/main/java/com/example/fragmentevaluable3/MainActivity.kt
+++ app/src/main/java/com/example/fragmentevaluable3/MainActivity.kt
@@ -1,56 +1,36 @@
 package com.example.fragmentevaluable3
 
 import android.os.Bundle
-import android.widget.ImageButton
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
 import com.example.fragmentevaluable3.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        remplazarFragement(Home())
 
-        binding.nav.setOnItemSelectedListener {
-          when(it.itemId){
+        // Mostrar el fragment Home al inicio
+        remplazarFragment(Home())
 
-            R.id.Main -> remplazarFragement(Home())
-            R.id.Favoritos -> remplazarFragement(Favoritos())
-            R.id.Login -> remplazarFragement(Login())
-
-
-              else ->{
-
-              }
-          }
+        // Configurar el BottomNavigationView
+        binding.nav.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.Main -> remplazarFragment(Home())
+                R.id.Favoritos -> remplazarFragment(Favoritos())
+                R.id.Login -> remplazarFragment(Login())
+            }
             true
         }
-
-        val btn1 = findViewById<ImageButton>(R.id.btnAnime1)
-        val btn2 = findViewById<ImageButton>(R.id.btnAnime2)
-        val btn3 = findViewById<ImageButton>(R.id.btnAnime3)
-        val btn4 = findViewById<ImageButton>(R.id.btnAnime4)
-
-        btn1.setOnClickListener {
-            setContentView(R.layout.descripcion_anime1)
-        }
-
-    }
-    private fun remplazarFragement(fragment: Fragment){
-
-        val fragmentManager = supportFragmentManager
-        val fragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.FrameLayout, fragment)
-        fragmentTransaction.commit()
     }
 
-
+    private fun remplazarFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.FrameLayout, fragment)
+            .commit()
+    }
 }
